260893b04cd62e5d954b3d09bb3b865feaadf5e3
lee <lee@lee>
2018-06-27 12:25:06 +0900

    member con mapper 추가 jsp 임시 하나 만듬

---
+++ spring_soldesk/src/main/java/org/soldesk/control/MemberController.java
@@ -0,0 +1,33 @@
+package org.soldesk.control;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.soldesk.DAO.CustomerDAO;
+import org.soldesk.DAO.MemberDAO;
+import org.soldesk.DAO.MemberDAOImpl;
+import org.soldesk.DTO.CustomerDTO;
+import org.soldesk.DTO.MemberDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	@Inject
+	CustomerDAO dao;
+
+	public void setDao(CustomerDAO dao) {
+		this.dao = dao;
+	}
+	
+	@RequestMapping(value="/MemberAll")
+	public String list(Model model) {
+		List<CustomerDTO> list = dao.CustomerSelectAll();
+		model.addAttribute("list", list);
+		
+		return "MemberAll";
+	}
+}
